eb7414e0dfd0e748f5991ab7e20f3008c25a4173
Nabi <ismailov.nabi2017@yandex.ru>
2023-04-17 03:54:08 +0400

    fix: remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+//const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+    let res = [];
+
+    if (l) {
+        l.next = removeKFromList(l.next, k);
+        if (l.value === k) {
+            res = l.next;
+        } else {
+            res = l;
+        }
+        return res;
+    }
+    return null;
 }
 
 module.exports = {
-  removeKFromList
+    removeKFromList
 };
